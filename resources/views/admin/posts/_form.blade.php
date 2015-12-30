a5b30f738b68a1136dc4768908ec39c15ec63c62
Ennio Simões <ennio.simoes@outlook.com>
2015-12-30 12:34:23 -0200

    Antes da lib image.intervention.io

--- resources/views/admin/posts/_form.blade.php
+++ resources/views/admin/posts/_form.blade.php
@@ -8,7 +8,11 @@
 </div>
 <div class="form-group">
     {!! Form::label('Imagem', 'Imagem:') !!}
-    {!! Form::text('post[imagem]', null, ['class'=>'form-control']) !!}
+    {!! Form::file('post[imagem]', ['id' => 'imageLoader']) !!}
+    <p class="help-block">Selecione a area para cortar</p>
+    <div style="width: 50%;">
+        <canvas id="imageCanvas"></canvas>
+    </div>
 </div>
 <div class="form-group">
     {!! Form::label('Descrição', 'Descrição:') !!}
@@ -30,3 +34,42 @@
     {!! Form::label('Status', 'Status:') !!}
     {!! Form::select('post[ativo]', [1 => 'ativo', 2 => 'inativo'], null, ['class'=>'form-control']) !!}
 </div>
+
+{{-- HIDDENS --}}
+    {!! Form::hidden('post[x1]') !!}
+    {!! Form::hidden('post[y1]') !!}
+    {!! Form::hidden('post[x2]') !!}
+    {!! Form::hidden('post[y2]') !!}
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#imageCanvas').imgAreaSelect({
+            onSelectEnd: function (img, selection) {
+                $('input[name="post[x1]"]').val(selection.x1);
+                $('input[name="post[y1]"]').val(selection.y1);
+                $('input[name="post[x2]"]').val(selection.x2);
+                $('input[name="post[y2]"]').val(selection.y2);
+            }
+        });
+    });
+</script>
+<script type="text/javascript">
+    var imageLoader = document.getElementById('imageLoader');
+        imageLoader.addEventListener('change', handleImage, false);
+    var canvas = document.getElementById('imageCanvas');
+    var ctx = canvas.getContext('2d');
+
+    function handleImage(e){
+        var reader = new FileReader();
+        reader.onload = function(event){
+            var img = new Image();
+            img.onload = function(){
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img,0,0);
+            }
+            img.src = event.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
+</script>
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,7 +5,7 @@
   <div class="col-xs-12">
     @include('errors._check')
     <br />
-    {!! Form::open(['route'=>'admin.posts.store']) !!}
+    {!! Form::open(['route'=>'admin.posts.store', 'files' => true]) !!}
         @include('admin.posts._form')
         <div class="form-group">
             {!! Form::submit('submit', ['class'=>'btn btn-primary']) !!}
