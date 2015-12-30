9b682169af130b5a2b52f3c711f6a4f82e93a79f
Ennio Simões <ennio.simoes@outlook.com>
2015-12-30 12:44:39 -0200

    Instalacao image.intervention.io OK

--- config/app.php
+++ config/app.php
@@ -138,6 +138,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         Illuminate\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -197,7 +198,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Illuminate\Html\FormFacade::class,
         'HTML' => Illuminate\Html\HtmlFacade::class,
-
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
